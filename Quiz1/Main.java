af32b46a0cb4b3655263bf8511ece91b747037f7
keryiing27 <81569360+keryiing27@users.noreply.github.com>
2021-04-15 13:50:14 +0800

    Add files via upload

---
+++ Quiz1/Main.java
@@ -0,0 +1,25 @@
+package Quiz1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Stationery pen = new Stationery();
+		
+		pen.setBrand("Faster");
+		pen.setColour("Blue");
+		pen.setPrice(1.50);
+		pen.setUsage("Writing");
+		
+		System.out.println("PEN PROPERTIES");
+		System.out.println("------------------------");
+		System.out.println("Brand :" + pen.getBrand());
+		System.out.println("Colour: " + pen.getColour());
+		System.out.println("Price: RM " + pen.getPrice());
+		System.out.println("Usage: "+ pen.getUsage());
+		System.out.println("------------------------");
+		
+		pen.Properties();
+
+	}
+
+}
---
+++ Quiz1/Stationery.java
@@ -0,0 +1,58 @@
+package Quiz1;
+
+import java.util.*;
+
+public class Stationery {
+	Scanner s = new Scanner(System.in);
+	
+	String colour;
+	String brand;
+	String usage;
+	double price;
+	
+	void Properties() {
+		System.out.println("Enter the brand: ");
+		brand = s.nextLine();
+		System.out.println("Enter the colour: ");
+		colour = s.nextLine();
+		System.out.println("Enter the usage: ");
+		usage = s.nextLine();
+		
+		System.out.println("STATIONERY PROPERTIES");
+		System.out.println("----------------------");
+		System.out.println("Brand: "+brand);
+		System.out.println("Colour: "+ colour);
+		System.out.println("Usage: " + usage);
+		System.out.printf("Price: RM %.2f",price);
+		
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public void setColour(String colour) {
+		this.colour = colour;
+
+	}
+	public void setUsage(String usage) {
+		this.usage = usage;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	
+	public String getBrand() {
+		return this.brand;
+	}
+	
+	public String getColour() {
+		return this.colour;
+	}
+	public String getUsage() {
+		return this.usage;
+	}
+	public double getPrice() {
+		return this.price;
+	}
+}
+
